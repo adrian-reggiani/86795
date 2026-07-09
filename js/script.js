a9ca498acfeda71da171ddd4c00f19df3ee71dc1
Adrian Reggiani <adriregg@gmail.com>
2026-07-09 14:55:17 -0300

    Se cambio la ruta del json porque el github no lo toma

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ let juego = [];
 
 // Cargar catalogo a traves de un json de manera dinamica
 async function cargarCatalogo() {
-    const respuesta = await fetch("../items.json");
+    const respuesta = await fetch("items.json");
     juego = await respuesta.json();
 
 //  Carga el catalogo de juegos en el Index.html
